97fdf8a851bb48700b7cd63f1c861f67327613ca
Maxwell Warncke <schlindo@gmail.com>
2022-11-21 19:43:11 -0500

    added login and signup activities

---
+++ app/src/main/java/com/example/team7project/Login.java
@@ -0,0 +1,14 @@
+package com.example.team7project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
