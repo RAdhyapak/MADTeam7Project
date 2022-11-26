e92420b37be5865e4fe0ef3f17323f2089170283
Maxwell Warncke <schlindo@gmail.com>
2022-11-26 15:09:04 -0500

    added profile activity and navbar functionality

--- app/src/main/java/com/example/team7project/Browse.java
+++ app/src/main/java/com/example/team7project/Browse.java
@@ -1,8 +1,10 @@
 package com.example.team7project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Browse extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Browse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        //navbar link to home activity
+        ImageButton buttonHome = findViewById(R.id.imageButtonHome);
+        buttonHome.setOnClickListener(view -> {
+            Intent intent = new Intent(Browse.this, Home.class);
+            startActivity(intent);
+        });
+
+        //navbar link to favorites activity
+        ImageButton buttonFavorites = findViewById(R.id.imageButtonFavorites);
+        buttonFavorites.setOnClickListener(view -> {
+            Intent intent = new Intent(Browse.this, Favorites.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/team7project/ProfilePage.java
@@ -0,0 +1,14 @@
+package com.example.team7project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
