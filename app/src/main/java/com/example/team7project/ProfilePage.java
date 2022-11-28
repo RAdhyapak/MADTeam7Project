1d72d530b7b07e3b64bcc82643e30f06bab17a13
Maxwell Warncke <schlindo@gmail.com>
2022-11-27 19:44:39 -0500

    spruced up the profile view

--- app/src/main/java/com/example/team7project/ProfilePage.java
+++ app/src/main/java/com/example/team7project/ProfilePage.java
@@ -1,14 +1,26 @@
 package com.example.team7project;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfilePage extends AppCompatActivity {
 
+    private TextView profileText, userIdText, emailText;
+    public String email, userId, profileName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        profileText = findViewById(R.id.profileText);
+        userIdText = findViewById(R.id.userIdText);
+        emailText = findViewById(R.id.emailText);
+
+        //TODO: get user info from db
+
     }
+
 }
